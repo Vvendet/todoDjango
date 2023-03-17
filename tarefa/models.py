14117caa2498bf27fa8fcb540643abc61cb54fb4
Vvendet <froees147@gmail.com>
2023-03-17 16:38:40 -0300

    primeiras alterações

--- tarefa/models.py
+++ tarefa/models.py
@@ -1,3 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class TarefaBd(models.Model):
+    conteudo=models.CharField(max_length=1000)
+    data=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
