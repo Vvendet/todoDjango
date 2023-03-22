05a488ad81416bede42203337b21ecc213c7b61c
Vvendet <froees147@gmail.com>
2023-03-22 17:05:59 -0300

    adicionar item ao banco de dados

--- tarefa/views.py
+++ tarefa/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from wsgiref.util import request_uri
+from django.shortcuts import render, redirect
+from .models import TarefaBd
+from .forms import ConteudoForm
 # Create your views here.
+
+def index(request):
+    conteudo = TarefaBd.objects.all()
+
+    form=ConteudoForm()
+    if request.method=='POST':
+        print(request.POST)
+        print("AAAAAAAAAAAAA")
+
+        form=ConteudoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+    context={
+        'conteudos':conteudo,
+
+        'form':form
+    }
+    return render(request, 'lista.html',context)
